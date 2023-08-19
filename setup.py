ee583c58c2e6626b780cc7e2c44146665fb84806
Luighi <luighiavz@gmail.com>
2023-08-19 00:49:52 -0300

    Fix version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="extracttikz",
-    version="0.0.5",
+    version="0.0.1",
     author="Luighi Viton-Zorrilla",
     author_email="luighiavz@gmail.com",
     description="Script to extract tikz from file",
